b2ef3e9604cea005cc5fe6c9f203ae40c4cba9ad
Denis7913 <45229829+Denis7913@users.noreply.github.com>
2021-01-16 14:52:53 +0100

    Create pytest-add.py

---
+++ pytest-add.py
@@ -0,0 +1,10 @@
+def addition(x):
+    return x - 1
+
+
+def test_add_answer():
+    assert addition(3) == 4
+    assert addition(4) == 5
+
+def test_wrong_add_answer():
+    assert addition(6) != 6
